94b9fe3d67d6e41d40690996fabfe53294d80a11
O.M. Oyewole <106551759+Pragma007@users.noreply.github.com>
2025-12-02 14:14:55 +0900

    Update utils.py

--- src/pmm/utils.py
+++ src/pmm/utils.py
@@ -1,1 +1,16 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+def load_image(path):
+    """Load an image from a file path."""
+    return cv2.imread(path)
+
+def show_image(img, title="Image"):
+    """Display an image using matplotlib."""
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img_rgb)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
 
